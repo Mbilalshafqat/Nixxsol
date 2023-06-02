db225f362b1c98c2683788ba864f9b4f0f586bb7
placeparks <86215469+placeparks@users.noreply.github.com>
2023-06-02 23:12:01 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import FirstPage from './components/FirstPage';
-import SecondPage from './components/SecondPage';
+import SecondPage from './components/SecondPage'; 
 import ThirdPage from './components/ThirdPage';
+import ForthPage from './components/ForthPage';
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from './components/Footer';
 import { MdFirstPage } from 'react-icons/md';
@@ -18,7 +20,8 @@ function App() {
           <Route exact path='/' element={<FirstPage />} />
           <Route exact path='/firstPage' element={<FirstPage />} />
           <Route exact path='/secondPage' element={<SecondPage />}/>
-                      <Route exact path='/thirdPage' element={<ThirdPage />}/>
+          <Route exact path='/thirdPage' element={<ThirdPage />}/>
+          <Route exact path='/forthPage' element={<ForthPage />}/>
 
         </Routes>
         <Footer/>
