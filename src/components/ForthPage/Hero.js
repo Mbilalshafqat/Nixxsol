15e303269d5b017aafcc76a030ce5a1cff96d50a
placeparks <86215469+placeparks@users.noreply.github.com>
2023-06-03 00:31:19 +0500

    Update Hero.js

--- src/components/ForthPage/Hero.js
+++ src/components/ForthPage/Hero.js
@@ -7,7 +7,7 @@ import styles from './Home.module.css';
 <>
 <div className="card my-5 p-5 no-hover" style={{width: "18rem;", border:"none", textAlign:"left", fontFamily:'Proxima Nova'}}>
   <div className="card-body">
-    <h1 className="card-title"style={{fontSize:"77px", fontWeight:"700"}}>Our<br/>Portfolio</h1>
+    <h1 className="card-title specific-card">Our<br/>Portfolio</h1>
     <p className="card-text" >Home/Portfolio</p>
     <p className="card-text" ><span style={{fontWeight:"700"}}>Filter by: All </span><span style={{color:"darkgrey"}}>/ Creative Design / Game Development / Web Development / Mobile Development</span></p>
   </div>
