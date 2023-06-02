843790b4188967b8c4c42588763f04e493989877
placeparks <86215469+placeparks@users.noreply.github.com>
2023-06-02 23:49:28 +0500

    Update Card.js

--- src/components/ForthPage/Card.js
+++ src/components/ForthPage/Card.js
@@ -1,19 +1,18 @@
 export default function Card(){
     return (
         <>
-        <div className="row row-cols-1 row-cols-md-2 g-4">
-        <div className="col">
-  <div className="card no-hover">
-    <img src="./Forth-page/rec1.png" className="card-img" alt="..."/>
-    <div class="card-img-overlay text-white" style={{textAlign:"left", paddingTop:"45%", width:"450px"}}>
-      <h5 class="card-title h4 h5-sm h3-md">Zombie Land</h5>
-      <p class="card-text text-sm">Mattis ornare tincidunt tempus mattis mi vel auctor ipsum venenatis. Tellus nunc scelerisque dictum sagittis euismod urna egestas quam. In ornare nisl leo odio magna.</p>
-    </div>
-  </div>
-</div>
-
-  <div className="col no-hover">
-  <div className="card no-hover">
+     <div className="row row-cols-1 row-cols-md-2 g-4">
+     <div className="col">
+            <div className="card no-hover">
+                <img src="./Forth-page/rec1.png" className="card-img img-fluid" alt="..."/>
+                <div class="card-img-overlay text-white" style={{textAlign:"left", paddingTop:"45%", width:"100%"}}>
+                <h5 class="card-title h4 h5-sm h3-md">Zombie Land</h5>
+                <p class="card-text text-sm">Mattis ornare tincidunt tempus mattis mi vel auctor ipsum venenatis. Tellus nunc scelerisque dictum sagittis euismod urna egestas quam. In ornare nisl leo odio magna.</p>
+                </div>
+            </div>
+        </div>
+        <div className="col"> 
+         <div className="card no-hover">
       <img src="./Forth-page/rec2.png" className="card-img-top" alt="..."/>
      </div>
   </div>
