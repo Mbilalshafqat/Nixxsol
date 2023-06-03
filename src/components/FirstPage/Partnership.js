e15ad5b796c73946a7b91a1a93aa5d8d1da27c33
placeparks <86215469+placeparks@users.noreply.github.com>
2023-06-03 17:21:52 +0500

    Update Partnership.js

--- src/components/FirstPage/Partnership.js
+++ src/components/FirstPage/Partnership.js
@@ -2,33 +2,44 @@ import {
     Flex,
     Box,
     Image,
-  } from "@chakra-ui/react";
-  
-  const Carousel = () => {
+    useBreakpointValue,
+} from "@chakra-ui/react";
+
+const Carousel = () => {
     const cards = [
-      { id: "1", src: "./First-page/image6.png" },
-      { id: "2", src: "./First-page/image7.png" },
-      { id: "3", src: "./First-page/image8.png" },
-      { id: "4", src: "./First-page/image9.png" },
-      { id: "5", src: "./First-page/image10.png" },
+        { id: "1", src: "./First-page/image6.png" },
+        { id: "2", src: "./First-page/image7.png" },
+        { id: "3", src: "./First-page/image8.png" },
+        { id: "4", src: "./First-page/image9.png" },
+        { id: "5", src: "./First-page/image10.png" },
     ];
+    const marginValue = useBreakpointValue({ base: "1rem", md: "1rem" });
     const cardsList = cards.map((item) => {
-      return (
-        <Box
-          key={item.id}
-          p="2rem"
-          borderRadius={"10px"}
-          boxShadow={"2px 2px 2px 2px rgba(0, 0, 0, 0.1)"}
-        >
-          <Image src={item.src} />
-        </Box>
-      );
+        return (
+            <Box
+                key={item.id}
+                p="2rem"
+                borderRadius={"10px"}
+                boxShadow={"2px 2px 2px 2px rgba(0, 0, 0, 0.1)"}
+                marginRight={marginValue}
+                marginBottom={marginValue}
+            >
+                <Image src={item.src} />
+            </Box>
+        );
     });
+
+    const justifyContentValue = useBreakpointValue({ base: "center", md: "space-between" });
+
     return (
-      <Flex p="10rem 3rem" gap="3rem" justifyContent={'space-between'} >
-        {cardsList}
-      </Flex>
+        <Flex
+            p="10rem 3rem"
+            justifyContent={justifyContentValue}
+            flexWrap="wrap"
+        >
+            {cardsList}
+        </Flex>
     );
-  };
-  
-  export default Carousel;
+};
+
+export default Carousel;
