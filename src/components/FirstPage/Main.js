22aa282652cc2e6812c62768727fe5de6b25a547
talha.qaisar <talhaqaisarvirk676@gamil.com>
2023-07-20 16:45:02 +0500

    changin navbar

--- src/components/FirstPage/Main.js
+++ src/components/FirstPage/Main.js
@@ -1,42 +1,46 @@
 import {
-    Button,
-    Flex,
-    Heading,
-    Image,
-    Stack,
-    Text,
-    useBreakpointValue
-  } from "@chakra-ui/react"
-  
-  export default function SplitScreen() {
-    return (
-        <Flex
+  Button,
+  Flex,
+  Heading,
+  Image,
+  Stack,
+  Text,
+  Grid,
+  useBreakpointValue
+} from "@chakra-ui/react"
+import React from "react"
+import Navbar from "../Navbar"
+
+export default function SplitScreen() {
+  return (
+    <>
+      <Grid
         w={'full'}
-        h={'70vh'}
+        h={'90vh'}
         backgroundImage={
           'url(./First-page/rectangle.png)'
         }
         backgroundSize={'cover'}
         backgroundPosition={'center center'}
-        fontFamily={'Proxima Nova'}>
-      <Stack minH={"60vh"} direction={{ base: "column", md: "row" }}>
-     
-        <Flex p={8} flex={1} align={"center"} justify={"center"}>
-          <Stack spacing={6} w={"full"} maxW={"lg"}>
-            
-            <Heading fontSize={{ base: "5xl", md: "xl", lg: "7xl" }} align={"left"}>
-            
-              <br />{" "}
-              <Text color={"white"} as={"span"}>
-              Bridging the gap between ideas and reality
-              </Text>{" "}
-            </Heading>
-        
-          </Stack>
-        </Flex>
-       
-      </Stack>
-      </Flex>
-    )
-  }
-  
+        templateRows={'repeat(5, 1fr)'}
+      >
+        <React.Fragment>
+          <Navbar />
+        </React.Fragment>
+        <Stack direction={{ base: "column", md: "row" }}>
+          <Flex px={24} flex={1} align={"center"}>
+            <Stack spacing={6} w={"full"} maxW={"lg"}>
+              <Heading fontSize={{ base: "5xl", md: "xl", lg: "67px" }} align={"left"}>
+                <br />{" "}
+                <Text color={"white"} as={"span"}>
+                  Bridging the gap between ideas and reality
+                </Text>{" "}
+              </Heading>
+            </Stack>
+          </Flex>
+        </Stack>
+      </Grid>
+    </>
+  )
+}
+
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,41 +19,35 @@ import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
 
 
 const linksData = [
-  { name: "Home", href: "/FirstPage" },
-  { name: "IT Services", href: "/SecondPage" },
-  { name: "About", href: "/ThirdPage" },
-  { name: "Portfolio", href: "/ForthPage" },
-  ];
+  { name: "Home", href: "" },
+  { name: "IT Services", href: "/ItServices" },
+  { name: "About", href: "/about" },
+  { name: "Portfolio", href: "/portfolio" },
+];
 
-export default function Simple() {
-  const { isOpen, onOpen, onClose } = useDisclosure();
+export default function Navbar() {
+  const { isOpen, onOpen, onClose } = useDisclosure()
 
-  const NavLink = ({ children, href }) => (
-    <Link
-      px={2}
-      py={1}
-      rounded={"md"}
-      _hover={{
-        textDecoration: "none",
-        bg: useColorModeValue("gray.200", "gray.700"),
-      }}
-      href={href}
-    >
-      {children}
-    </Link>
-  );
+  // const HomeButton = ({ children, href }) => {
+  //   const path = window.location.pathname;
+  //   return path === "/SecondPage" || path === "/ForthPage" || path === "/ThirdPage" || path === "/FifthPage" ? (
+  //     <NavLink children={children} href={href} />
+  //   ) : null;
+  // };
 
-  const HomeButton = ({ children, href }) => {
+  const path = (link) => {
     const path = window.location.pathname;
-    return path === "/SecondPage" || path === "/ForthPage" || path === "/ThirdPage" || path === "/FifthPage"  ? (
-      <NavLink children={children} href={href} />
-    ) : null;
-  };
+    console.log(link)
+    if (path === "" && link === "") return null
+    else return link
+  }
 
   return (
     <>
-      <Box bg={useColorModeValue("white", "gray.800")} px={4}>
-        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+      <Box
+      //bg={useColorModeValue("white", "gray.800")} px={4}
+      >
+        {/* <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
           <IconButton
             size={"md"}
             icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
@@ -63,7 +57,7 @@ export default function Simple() {
           />
           <HStack spacing={8} alignItems={"center"}>
             <Box>
-              <img src='./logo.svg' alt=""/>
+              <img src='./logo.svg' alt="" />
             </Box>
             <HStack
               as={"nav"}
@@ -102,26 +96,63 @@ export default function Simple() {
               </MenuList>
             </Menu>
           </Flex>
-        </Flex>
+        </Flex> */}
 
-        {isOpen ? (
+        {/* -------------------------- NAVBAR FIGMA -----------------------*/}
+
+        <div className="h-[70px] bg-transparent mx-24 border text-white flex justify-between place-items-center">
+          <div className="flex place-items-center gap-2">
+            <img className="h-[43px]" src="./First-Page//Vector.png" alt="..." />
+            <img className="h-[22px]" src="./First-Page//Group 2.png" alt="..." />
+            {/* <img src="./First-Page//nix-logo.JPG" />jk */}
+          </div>
+          <div className="text-white flex justify-between gap-8 lg:text-sm font-normal">
+            {
+              linksData.map((list, index) => {
+                return (
+                  <>
+                    <span className="text-decoration-none text-white" key={index}>
+                      <Link to={() => path(list.href)}>
+                        {list.name}
+                      </Link>
+                    </span>
+                  </>
+                )
+              })
+            }
+          </div>
+          <div>
+            <span>hell</span>
+          </div>
+        </div>
+
+
+
+        {/* -------------------------- NAVBAR FIGMA -----------------------*/}
+
+
+
+
+
+
+        {/* {isOpen ? (
           <Box pb={4} display={{ md: "none" }}>
             <Stack as={"nav"} spacing={4}>
               {linksData.map(link => (
                 <NavLink key={link.name} href={link.href}>{link.name}</NavLink>
               ))}
               <Menu>
-               <MenuButton rounded={"md"} cursor={"pointer"}>
+                <MenuButton rounded={"md"} cursor={"pointer"}>
                   Services&#9660;
                 </MenuButton>
                 <MenuList>
                   <MenuItem as={Link} href="/fifthPage">UI/UX Designs</MenuItem>
                   <MenuItem as={Link} href="/sixthPage">Game Development</MenuItem>
-                  </MenuList>
+                </MenuList>
               </Menu>
             </Stack>
           </Box>
-        ) : null}
+        ) : null} */}
       </Box>
     </>
   );
